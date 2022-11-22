2d0236fd675c2530a36cd37a399c7bd3f9016326
Luc <iriela.rasamo3ly@gmail.com>
2022-11-22 09:33:05 +0100

    Adding name to product

--- SampleExport/Models/Product.cs
+++ SampleExport/Models/Product.cs
@@ -13,15 +13,19 @@ namespace SampleExport.Models
         [DataMember(Name = "id")]
         public Guid Id { get; set; }
 
+        [DataMember(Name = "name")]
+        public string Name { get; set; }
+
         [DataMember(Name = "price")]
         public decimal Price { get; set; }
 
         [DataMember(Name = "qty")]
         public int Quantity { get; set; }
 
-        public Product(Guid id, decimal price, int quantity)
+        public Product(Guid id,string name, decimal price, int quantity)
         {
             Id = id;
+            Name = name;
             Price = price;
             Quantity = quantity;
         }
